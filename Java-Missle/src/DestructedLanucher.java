2b3202895fdeaaa00a96c8cccdf6a8191f31e170
avishayMahluf <avishay89@gmail.com>
2014-07-15 17:10:57 +0300

    .

--- Java-Missle/src/DestructedLanucher.java
+++ Java-Missle/src/DestructedLanucher.java
@@ -1,9 +1,9 @@
 
-public class destructedLanucher extends Thread{
+public class DestructedLanucher extends Thread{
 	private String id;
 	private String destructTime;
 	
-	public destructedLanucher(String id, String destructTime) {
+	public DestructedLanucher(String id, String destructTime) {
 		super();
 		this.id = id;
 		this.destructTime = destructTime;
