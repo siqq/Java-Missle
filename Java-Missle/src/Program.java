6474435bbf86e3b41cba6aa2e067e8c414558608
avishayMahluf <avishay89@gmail.com>
2014-07-17 20:34:14 +0300

    .

--- Java-Missle/src/Program.java
+++ Java-Missle/src/Program.java
@@ -1,14 +1,21 @@
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.xml.sax.SAXException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 
 public class Program {
 	public static void main(String[]args) {
 		try {
 			War war = new War();
+			LocalDateTime ldate = LocalDateTime.now();
+			DateTimeFormatter date = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+			System.out.println(ldate.format(date));
 		} catch (ParserConfigurationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
