0cb959640668c140bd5cc3f7df36f0b5e4fe8de4
dvirsofer <dvir.sofer90@gmail.com>
2014-07-29 12:09:35 +0300

    Constatns

---
+++ Java-Missle/src/Constatns.java
@@ -0,0 +1,6 @@
+
+public class Constatns {
+	
+	public static final int TIME_INTERVAL = 1000; 	//sleep time for threads
+	public static final double SUCCESS_RATE = 0.2;	//success rate for destructors
+}
