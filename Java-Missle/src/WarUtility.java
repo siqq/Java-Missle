e8132bccbaa15f1ff34530dbb59a38785d31730a
dvirsofer <dvir.sofer90@gmail.com>
2014-08-02 13:13:08 +0300

    warUtility

---
+++ Java-Missle/src/WarUtility.java
@@ -0,0 +1,43 @@
+import java.util.Vector;
+
+
+public class WarUtility {
+
+	/**
+	 * get id and search for missile with that id in missileLaunchers vector
+	 * @param id
+	 * @return wanted Missile from war
+	 */
+	public static Missile getMissileById(String id, War war) {
+		Vector<Launcher> missileLaunchers = war.getMissileLaunchers();
+		int size_launcher = missileLaunchers.size();
+		for (int i = 0; i < size_launcher; i++) {
+			Launcher l = missileLaunchers.elementAt(i);
+			int size_missile = l.getMissiles().size();
+			for (int j=0; j < size_missile; j++){
+				Missile m = l.getMissiles().elementAt(j);
+				if (id.equals(m.getMissileId())) {
+					return m;
+				}
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * get id and search for Launcher with that id in missileLaunchers vector
+	 * @param id
+	 * @return wanted Launcher from war
+	 */
+	public static Launcher getLauncherById(String id, War war) {
+		Vector<Launcher> missileLaunchers = war.getMissileLaunchers();
+		int size_launcher = missileLaunchers.size();
+		for (int i = 0; i < size_launcher; i++) {
+			Launcher l = missileLaunchers.elementAt(i);
+			if (id.equals(l.getLauncherId())) {
+				return l;
+			}
+		}
+		return null;
+	}
+}
