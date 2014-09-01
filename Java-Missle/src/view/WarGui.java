5429e61d31d08721219859a0a60b24b6dbf6c064
GalKarp <galkarp@gmail.com>
2014-09-01 15:23:57 +0300

    ypdate

--- Java-Missle/src/view/WarGui.java
+++ Java-Missle/src/view/WarGui.java
@@ -12,31 +12,81 @@ import javax.swing.JPopupMenu;
 import java.awt.Component;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.JSplitPane;
+import java.awt.GridLayout;
+import javax.swing.SpringLayout;
+import javax.swing.border.MatteBorder;
 
 public class WarGui extends JFrame {
 	public WarGui() {
-		setSize(1046, 489);
+		setSize(1180, 693);
 		setResizable(false);
 		setTitle("War");
-		getContentPane().setLayout(null);
+		SpringLayout springLayout = new SpringLayout();
+		getContentPane().setLayout(springLayout);
 		
-		ControlPanel controlPanel = new ControlPanel();
-		OrefPanel orefPanel = new OrefPanel();
-		ProgressPanel progressPanel = new ProgressPanel();
-		LaunchersPanel launchersPanel = new LaunchersPanel();
-		IronDomesPanel ironDomesPanel = new IronDomesPanel();
-		DestroyersPanel destroyersPanel = new DestroyersPanel();
+		JPanel panel = new JPanel();
+		springLayout.putConstraint(SpringLayout.NORTH, panel, 10, SpringLayout.NORTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, panel, 10, SpringLayout.WEST, getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, panel, -10, SpringLayout.SOUTH, getContentPane());
+		panel.setBorder(new LineBorder(new Color(0, 0, 0)));
+		getContentPane().add(panel);
 		
-		getContentPane().add(controlPanel);
-		getContentPane().add(orefPanel);
-		getContentPane().add(progressPanel);
-		getContentPane().add(launchersPanel);
-		getContentPane().add(ironDomesPanel);
-		getContentPane().add(destroyersPanel);
+		JPanel panel_1 = new JPanel();
+		springLayout.putConstraint(SpringLayout.NORTH, panel_1, 10, SpringLayout.NORTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, panel_1, 324, SpringLayout.WEST, getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, panel_1, -10, SpringLayout.SOUTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, panel_1, -241, SpringLayout.EAST, getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, panel, -6, SpringLayout.WEST, panel_1);
+		SpringLayout sl_panel = new SpringLayout();
+		panel.setLayout(sl_panel);
+		
+		JPanel panel_3 = new JPanel();
+		sl_panel.putConstraint(SpringLayout.NORTH, panel_3, 12, SpringLayout.NORTH, panel);
+		sl_panel.putConstraint(SpringLayout.WEST, panel_3, 12, SpringLayout.WEST, panel);
+		sl_panel.putConstraint(SpringLayout.SOUTH, panel_3, -407, SpringLayout.SOUTH, panel);
+		sl_panel.putConstraint(SpringLayout.EAST, panel_3, -8, SpringLayout.EAST, panel);
+		panel_3.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panel.add(panel_3);
+		
+		JPanel panel_5 = new JPanel();
+		sl_panel.putConstraint(SpringLayout.WEST, panel_5, 9, SpringLayout.WEST, panel);
+		sl_panel.putConstraint(SpringLayout.SOUTH, panel_5, -10, SpringLayout.SOUTH, panel);
+		sl_panel.putConstraint(SpringLayout.EAST, panel_5, -7, SpringLayout.EAST, panel);
+		panel_5.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panel.add(panel_5);
+		
+		JPanel panel_4 = new JPanel();
+		sl_panel.putConstraint(SpringLayout.NORTH, panel_5, 7, SpringLayout.SOUTH, panel_4);
+		sl_panel.putConstraint(SpringLayout.NORTH, panel_4, 7, SpringLayout.SOUTH, panel_3);
+		sl_panel.putConstraint(SpringLayout.WEST, panel_4, -296, SpringLayout.EAST, panel);
+		sl_panel.putConstraint(SpringLayout.SOUTH, panel_4, 230, SpringLayout.SOUTH, panel_3);
+		sl_panel.putConstraint(SpringLayout.EAST, panel_4, -7, SpringLayout.EAST, panel);
+		panel_4.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panel.add(panel_4);
+		panel_1.setBorder(new LineBorder(new Color(0, 0, 0)));
+		getContentPane().add(panel_1);
+		
+		JPanel panel_2 = new JPanel();
+		springLayout.putConstraint(SpringLayout.WEST, panel_2, 6, SpringLayout.EAST, panel_1);
+		springLayout.putConstraint(SpringLayout.NORTH, panel_2, 100, SpringLayout.NORTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, panel_2, -10, SpringLayout.EAST, getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, panel_2, 654, SpringLayout.NORTH, getContentPane());
+		panel_2.setBorder(new LineBorder(new Color(0, 0, 0)));
+		getContentPane().add(panel_2);
+		SpringLayout sl_panel_2 = new SpringLayout();
+		panel_2.setLayout(sl_panel_2);
+		
+		JPanel panel_6 = new JPanel();
+		panel_6.setBorder(new LineBorder(new Color(0, 0, 0)));
+		springLayout.putConstraint(SpringLayout.NORTH, panel_6, 0, SpringLayout.NORTH, panel);
+		springLayout.putConstraint(SpringLayout.WEST, panel_6, 6, SpringLayout.EAST, panel_1);
+		springLayout.putConstraint(SpringLayout.SOUTH, panel_6, -6, SpringLayout.NORTH, panel_2);
+		springLayout.putConstraint(SpringLayout.EAST, panel_6, 231, SpringLayout.EAST, panel_1);
+		getContentPane().add(panel_6);
 	
 		setVisible(true);
 		
 		
 	}
-
 }
