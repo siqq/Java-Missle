d51bad1335870728baa40bcaab5e35c6678fd584
GalKarp <galkarp@gmail.com>
2014-09-02 01:30:06 +0300

    Tired of this shit

--- Java-Missle/src/view/ProgressPanel.java
+++ Java-Missle/src/view/ProgressPanel.java
@@ -1,31 +1,58 @@
 package view;
 
 import java.awt.Color;
-import java.awt.Container;
+import java.awt.GridLayout;
+import java.util.LinkedList;
+import java.util.Queue;
 
 import javax.swing.JPanel;
-import javax.swing.SpringLayout;
+import javax.swing.JProgressBar;
 import javax.swing.border.LineBorder;
 
+
 public class ProgressPanel extends JPanel {
+	private Queue <JProgressBar> progressBars = new LinkedList<JProgressBar>();	        
 	public ProgressPanel() {
-		this.setBorder(new LineBorder(new Color(0, 0, 0)));
+	    	this.setBorder(new LineBorder(new Color(0, 0, 0)));
+	    	setLayout(new GridLayout(10, 1, 0, 0));
+//	    	progressBars = new LinkedList<JProgressBar>();
+		    
+
+		
+	    	for(int progressBarNo=0 ; progressBarNo < 10 ; progressBarNo++){
+	    	    
+	    	    progressBars.add(new JProgressBar());
+	    	    this.add(progressBars.peek());
+	    	}
+//		JProgressBar progressBar_1 = new JProgressBar();
+//		add(progressBar_1);
+//		JProgressBar progressBar_2 = new JProgressBar();
+//		add(progressBar_2);
+		
+//		JProgressBar progressBar_3 = new JProgressBar();
+//		this.add(progressBar_3);
+//		
+//		JProgressBar progressBar_4 = new JProgressBar();
+//		this.add(progressBar_4);
+//		
+//		JProgressBar progressBar_5 = new JProgressBar();
+//		this.add(progressBar_5);
+//		
+//		JProgressBar progressBar_6 = new JProgressBar();
+//		this.add(progressBar_6);
+//		
+//		JProgressBar progressBar_7 = new JProgressBar();
+//		this.add(progressBar_7);
+//		
+//		JProgressBar progressBar_8 = new JProgressBar();
+//		this.add(progressBar_8);
+//		
+//		JProgressBar progressBar_9 = new JProgressBar();
+//		this.add(progressBar_9);
+//		
+//		JProgressBar progressBar = new JProgressBar();
+//		this.add(progressBar);
 
 		
 	}
 }
-
-/*
-
-		JProgressBar progressBar_1 = new JProgressBar();
-		progressPanel.add(progressBar_1);
-		
-		JProgressBar progressBar_2 = new JProgressBar();
-		progressPanel.add(progressBar_2);
-		
-		JProgressBar progressBar_3 = new JProgressBar();
-		progressPanel.add(progressBar_3);
-		
-		JProgressBar progressBar = new JProgressBar();
-		progressPanel.add(progressBar);
-*/
--- Java-Missle/src/view/WarGui.java
+++ Java-Missle/src/view/WarGui.java
@@ -1,22 +1,13 @@
 package view;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import java.awt.BorderLayout;
-import javax.swing.border.TitledBorder;
-import javax.swing.border.BevelBorder;
-import javax.swing.border.LineBorder;
 import java.awt.Color;
-import javax.swing.JProgressBar;
-import javax.swing.JPopupMenu;
-import java.awt.Component;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import javax.swing.JSplitPane;
+import java.awt.Font;
 import java.awt.GridLayout;
-import javax.swing.SpringLayout;
-import javax.swing.border.MatteBorder;
+
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.SpringLayout;
+import javax.swing.SwingConstants;
 
 public class WarGui extends JFrame {
 	public WarGui() {
@@ -24,48 +15,37 @@ public class WarGui extends JFrame {
 		SpringLayout springLayout = new SpringLayout();
 		
 		ControlPanel controlPanel = new ControlPanel();
+		springLayout.putConstraint(SpringLayout.EAST, controlPanel, -10, SpringLayout.EAST, getContentPane());
 		DestroyersPanel destroyersPanel = new DestroyersPanel();
+		springLayout.putConstraint(SpringLayout.WEST, destroyersPanel, 10, SpringLayout.WEST, getContentPane());
 		IronDomesPanel ironDomesPanel = new IronDomesPanel();
+		springLayout.putConstraint(SpringLayout.WEST, ironDomesPanel, 10, SpringLayout.WEST, getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, ironDomesPanel, -856, SpringLayout.EAST, getContentPane());
 		LaunchersPanel launchersPanel = new LaunchersPanel();
+		springLayout.putConstraint(SpringLayout.WEST, launchersPanel, 10, SpringLayout.WEST, getContentPane());
 		ProgressPanel progressPanel = new ProgressPanel();
-		OrefPanel orefPanel = new OrefPanel();		
+		springLayout.putConstraint(SpringLayout.EAST, launchersPanel, -6, SpringLayout.WEST, progressPanel);
+		springLayout.putConstraint(SpringLayout.EAST, destroyersPanel, -6, SpringLayout.WEST, progressPanel);
+		springLayout.putConstraint(SpringLayout.WEST, controlPanel, 6, SpringLayout.EAST, progressPanel);
+		springLayout.putConstraint(SpringLayout.WEST, progressPanel, 6, SpringLayout.EAST, ironDomesPanel);
+		springLayout.putConstraint(SpringLayout.SOUTH, progressPanel, -10, SpringLayout.SOUTH, getContentPane());
 		
-
-		// SpringLayout for Control Panel
-		springLayout.putConstraint(SpringLayout.EAST , controlPanel, -10, SpringLayout.EAST, getContentPane());
-		springLayout.putConstraint(SpringLayout.WEST , controlPanel, 6, SpringLayout.EAST, progressPanel);
+				// SpringLayout for Progress Panel
+		springLayout.putConstraint(SpringLayout.EAST , progressPanel, -241, SpringLayout.EAST, getContentPane());
+		OrefPanel orefPanel = new OrefPanel();		
+		springLayout.putConstraint(SpringLayout.WEST, orefPanel, 6, SpringLayout.EAST, progressPanel);
+		springLayout.putConstraint(SpringLayout.EAST, orefPanel, -10, SpringLayout.EAST, getContentPane());
+		springLayout.putConstraint(SpringLayout.NORTH, progressPanel, 42, SpringLayout.NORTH, orefPanel);
 		springLayout.putConstraint(SpringLayout.NORTH, controlPanel, 100, SpringLayout.NORTH, getContentPane());
 		springLayout.putConstraint(SpringLayout.SOUTH, controlPanel, 654, SpringLayout.NORTH, getContentPane());
-
-		// SpringLayout for Destroyers Panel
-		springLayout.putConstraint(SpringLayout.EAST , destroyersPanel, -6, SpringLayout.WEST, progressPanel);
-		springLayout.putConstraint(SpringLayout.WEST , destroyersPanel, 10, SpringLayout.WEST, getContentPane());	
 		springLayout.putConstraint(SpringLayout.NORTH, destroyersPanel, 503, SpringLayout.NORTH, getContentPane());
 		springLayout.putConstraint(SpringLayout.SOUTH, destroyersPanel, -10, SpringLayout.SOUTH, getContentPane());
-
-		// SpringLayout for Iron Domes Panel
-		springLayout.putConstraint(SpringLayout.EAST , ironDomesPanel, -6, SpringLayout.WEST, progressPanel);
-		springLayout.putConstraint(SpringLayout.WEST , ironDomesPanel, 10, SpringLayout.WEST, getContentPane());
 		springLayout.putConstraint(SpringLayout.NORTH, ironDomesPanel, 6, SpringLayout.SOUTH, launchersPanel);
 		springLayout.putConstraint(SpringLayout.SOUTH, ironDomesPanel, -6, SpringLayout.NORTH, destroyersPanel);
-
-		// SpringLayout for Launchers Panel
-		springLayout.putConstraint(SpringLayout.EAST , launchersPanel, -6, SpringLayout.WEST, progressPanel);
-		springLayout.putConstraint(SpringLayout.WEST , launchersPanel, 10, SpringLayout.WEST, getContentPane());
 		springLayout.putConstraint(SpringLayout.NORTH, launchersPanel, 10, SpringLayout.NORTH, getContentPane());
 		springLayout.putConstraint(SpringLayout.SOUTH, launchersPanel, -410, SpringLayout.SOUTH, getContentPane());
-
-		// SpringLayout for Oref Panel
-		springLayout.putConstraint(SpringLayout.EAST , orefPanel, -10, SpringLayout.EAST, getContentPane());
-		springLayout.putConstraint(SpringLayout.WEST , orefPanel, 6, SpringLayout.EAST, progressPanel);
 		springLayout.putConstraint(SpringLayout.NORTH, orefPanel, 10, SpringLayout.NORTH, getContentPane());
 		springLayout.putConstraint(SpringLayout.SOUTH, orefPanel, -6, SpringLayout.NORTH, controlPanel);
-
-		// SpringLayout for Progress Panel
-		springLayout.putConstraint(SpringLayout.EAST , progressPanel, -241, SpringLayout.EAST, getContentPane());
-		springLayout.putConstraint(SpringLayout.WEST , progressPanel, 324, SpringLayout.WEST, getContentPane());
-		springLayout.putConstraint(SpringLayout.NORTH, progressPanel, 0, SpringLayout.NORTH, orefPanel);
-		springLayout.putConstraint(SpringLayout.SOUTH, progressPanel, -10, SpringLayout.SOUTH, getContentPane());
 		
 
 		// Add all panels to the main Frame
@@ -80,6 +60,20 @@ public class WarGui extends JFrame {
 		// Main Frame Settings
 		setTitle("War");
 		getContentPane().setLayout(springLayout);		
+		
+		//Jlabel for the progressbar
+		JLabel lblEnemyMissileInterception = new JLabel("ENEMY MISSILE INTERCEPTION STATUS");
+		lblEnemyMissileInterception.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblEnemyMissileInterception.setHorizontalAlignment(SwingConstants.CENTER);
+		lblEnemyMissileInterception.setBackground(Color.WHITE);
+		lblEnemyMissileInterception.setForeground(Color.BLACK);
+		springLayout.putConstraint(SpringLayout.NORTH, lblEnemyMissileInterception, 0, SpringLayout.NORTH, orefPanel);
+		springLayout.putConstraint(SpringLayout.WEST, lblEnemyMissileInterception, 6, SpringLayout.EAST, launchersPanel);
+		springLayout.putConstraint(SpringLayout.SOUTH, lblEnemyMissileInterception, -6, SpringLayout.NORTH, progressPanel);
+		springLayout.putConstraint(SpringLayout.EAST, lblEnemyMissileInterception, 0, SpringLayout.EAST, progressPanel);
+		
+		
+		getContentPane().add(lblEnemyMissileInterception);
 		setSize(1180, 693);
 		setResizable(false);		
 		setVisible(true);
