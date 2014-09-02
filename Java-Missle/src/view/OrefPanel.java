68e91b50bacd19bd3ed1b9342f0b067dc9bceddd
siqq <andruxa88@gmail.com>
2014-09-02 14:27:34 +0300

    oref design

--- Java-Missle/src/view/OrefPanel.java
+++ Java-Missle/src/view/OrefPanel.java
@@ -4,10 +4,45 @@ import java.awt.Color;
 
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
+import javax.swing.SpringLayout;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridLayout;
+
+import javax.swing.JTextField;
 
 public class OrefPanel extends JPanel {
+	public static final String OREF_IMAGE_PATH = "/drawable/orefImage225x70.png";
+	private JTextField alert1Text;
+	private JTextField alert2Text;
+	private JTextField alert3Text;
 	public OrefPanel() {
-		this.setBorder(new LineBorder(new Color(0, 0, 0)));
+		setBackground(Color.DARK_GRAY);
+		this.setBorder(null);
+		setLayout(new GridLayout(4, 1));
+		
+		JLabel orefImageLable = new JLabel();
+		orefImageLable.setIcon(new ImageIcon(OrefPanel.class.getResource(OREF_IMAGE_PATH)));
+		add(orefImageLable);
+		
+		alert1Text = new JTextField();
+		alert1Text.setBackground(new Color(255, 127, 80));
+		add(alert1Text);
+		alert1Text.setColumns(10);
+		
+		alert2Text = new JTextField();
+		alert2Text.setBackground(new Color(255, 127, 80));
+		add(alert2Text);
+		alert2Text.setColumns(10);
+		
+		alert3Text = new JTextField();
+		alert3Text.setBackground(new Color(255, 127, 80));
+		add(alert3Text);
+		alert3Text.setColumns(10);
 
 		}
 }
--- Java-Missle/src/view/WarGui.java
+++ Java-Missle/src/view/WarGui.java
@@ -11,10 +11,11 @@ import javax.swing.SwingConstants;
 
 public class WarGui extends JFrame {
 	public WarGui() {
-		setBackground(Color.BLACK);
+		setBackground(Color.DARK_GRAY);
 
 		SpringLayout springLayout = new SpringLayout();
 		ControlPanel controlPanel = new ControlPanel();
+		springLayout.putConstraint(SpringLayout.NORTH, controlPanel, 233, SpringLayout.NORTH, getContentPane());
 		springLayout.putConstraint(SpringLayout.EAST, controlPanel, -10, SpringLayout.EAST, getContentPane());
 		DestroyersPanel destroyersPanel = new DestroyersPanel();
 		springLayout.putConstraint(SpringLayout.WEST, destroyersPanel, 10, SpringLayout.WEST, getContentPane());
@@ -24,19 +25,19 @@ public class WarGui extends JFrame {
 		LaunchersPanel launchersPanel = new LaunchersPanel();
 		springLayout.putConstraint(SpringLayout.WEST, launchersPanel, 10, SpringLayout.WEST, getContentPane());
 		ProgressPanel progressPanel = new ProgressPanel();
+		springLayout.putConstraint(SpringLayout.SOUTH, progressPanel, -10, SpringLayout.SOUTH, getContentPane());
 		springLayout.putConstraint(SpringLayout.EAST, launchersPanel, -6, SpringLayout.WEST, progressPanel);
 		springLayout.putConstraint(SpringLayout.EAST, destroyersPanel, -6, SpringLayout.WEST, progressPanel);
 		springLayout.putConstraint(SpringLayout.WEST, controlPanel, 6, SpringLayout.EAST, progressPanel);
 		springLayout.putConstraint(SpringLayout.WEST, progressPanel, 6, SpringLayout.EAST, ironDomesPanel);
-		springLayout.putConstraint(SpringLayout.SOUTH, progressPanel, -10, SpringLayout.SOUTH, getContentPane());
 		
 				// SpringLayout for Progress Panel
 		springLayout.putConstraint(SpringLayout.EAST , progressPanel, -241, SpringLayout.EAST, getContentPane());
 		OrefPanel orefPanel = new OrefPanel();		
+		springLayout.putConstraint(SpringLayout.NORTH, orefPanel, 10, SpringLayout.NORTH, getContentPane());
 		springLayout.putConstraint(SpringLayout.WEST, orefPanel, 6, SpringLayout.EAST, progressPanel);
+		springLayout.putConstraint(SpringLayout.SOUTH, orefPanel, -6, SpringLayout.NORTH, controlPanel);
 		springLayout.putConstraint(SpringLayout.EAST, orefPanel, -10, SpringLayout.EAST, getContentPane());
-		springLayout.putConstraint(SpringLayout.NORTH, progressPanel, 42, SpringLayout.NORTH, orefPanel);
-		springLayout.putConstraint(SpringLayout.NORTH, controlPanel, 100, SpringLayout.NORTH, getContentPane());
 		springLayout.putConstraint(SpringLayout.SOUTH, controlPanel, 654, SpringLayout.NORTH, getContentPane());
 		springLayout.putConstraint(SpringLayout.NORTH, destroyersPanel, 503, SpringLayout.NORTH, getContentPane());
 		springLayout.putConstraint(SpringLayout.SOUTH, destroyersPanel, -10, SpringLayout.SOUTH, getContentPane());
@@ -44,8 +45,6 @@ public class WarGui extends JFrame {
 		springLayout.putConstraint(SpringLayout.SOUTH, ironDomesPanel, -6, SpringLayout.NORTH, destroyersPanel);
 		springLayout.putConstraint(SpringLayout.NORTH, launchersPanel, 10, SpringLayout.NORTH, getContentPane());
 		springLayout.putConstraint(SpringLayout.SOUTH, launchersPanel, -410, SpringLayout.SOUTH, getContentPane());
-		springLayout.putConstraint(SpringLayout.NORTH, orefPanel, 10, SpringLayout.NORTH, getContentPane());
-		springLayout.putConstraint(SpringLayout.SOUTH, orefPanel, -6, SpringLayout.NORTH, controlPanel);
 		
 
 		// Add all panels to the main Frame
@@ -63,13 +62,14 @@ public class WarGui extends JFrame {
 		
 		//Jlabel for the progressbar
 		JLabel lblEnemyMissileInterception = new JLabel("ENEMY MISSILE INTERCEPTION STATUS");
+		springLayout.putConstraint(SpringLayout.NORTH, lblEnemyMissileInterception, 10, SpringLayout.NORTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, lblEnemyMissileInterception, -618, SpringLayout.SOUTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.NORTH, progressPanel, 6, SpringLayout.SOUTH, lblEnemyMissileInterception);
 		lblEnemyMissileInterception.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		lblEnemyMissileInterception.setHorizontalAlignment(SwingConstants.CENTER);
 		lblEnemyMissileInterception.setBackground(Color.WHITE);
 		lblEnemyMissileInterception.setForeground(Color.BLACK);
-		springLayout.putConstraint(SpringLayout.NORTH, lblEnemyMissileInterception, 0, SpringLayout.NORTH, orefPanel);
 		springLayout.putConstraint(SpringLayout.WEST, lblEnemyMissileInterception, 6, SpringLayout.EAST, launchersPanel);
-		springLayout.putConstraint(SpringLayout.SOUTH, lblEnemyMissileInterception, -6, SpringLayout.NORTH, progressPanel);
 		springLayout.putConstraint(SpringLayout.EAST, lblEnemyMissileInterception, 0, SpringLayout.EAST, progressPanel);
 		
 		
