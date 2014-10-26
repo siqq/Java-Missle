3772eb60db45e842fb9b1e58a204f2a3824b67dc
siqq <andruxa88@gmail.com>
2014-10-26 16:12:02 +0300

    f

--- Java-Missle/src/main/Program.java
+++ Java-Missle/src/main/Program.java
@@ -18,8 +18,8 @@ public class Program {
      */
 	public static final String 			LAUNCHER = "launcher";
 	public static final String 			MISSILE = "missile";
-	public static final int 			TAKES_TIME_MIN = 1;
-	public static final int 			TAKES_TIME_MAX = 10;
+	public static final int 			TAKES_TIME_MIN = 4;
+	public static final int 			TAKES_TIME_MAX = 6;
 
 	public static void main(String[] args) {
 		
@@ -27,13 +27,11 @@ public class Program {
 			War warModel = new War();
 			AbstractWarView viewGui = new WarGui();	
 			WarController controller = new WarController(warModel,viewGui);
-			XMLparser xml = new XMLparser(controller, warModel);
 			
+			
+			new XMLparser(controller, warModel);			
 		    new WarDBConnection();
-			
-//		    new Server();
-
-			
+		    
 		} catch (ParserConfigurationException e) {
 			e.printStackTrace();
 		} catch (SAXException e) {
